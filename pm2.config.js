48eea78b96b82d9a72c5fc5132620a4a5783fab8
qiankai <qiankai@magictavern.com>
2024-12-13 12:43:50 +0800

    Upgrade sharp to support windows Ubuntu

--- pm2.config.js
+++ pm2.config.js
@@ -1,17 +1,12 @@
 module.exports = {
     apps: [{
-        name: "wechat-pal",
+        name: "lark-pal",
         script: "./index.js",
         cwd: "./",
         log_file: "./logs/pm2.log",
-        restart_delay: 10000,
-        env: {
-            WECHATY_PUPPET_SERVICE_AUTHORITY: "token-service-discovery-test.juzibot.com",
-            WECHATY_SERVICE_DISCOVERY_ENDPOINT: "https://token-service-discovery-test.juzibot.com",
-            WECHATY_PUPPET_SERVICE_NO_TLS_INSECURE_CLIENT: "true"
-        }
+        restart_delay: 10000
     }, {
-        name: "wechat-pal-log-watcher",
+        name: "lark-pal-log-watcher",
         script: "./log-watcher.js",
         cwd: "./",
         log_file: "./logs/log-watcher.log",
